e27d5c1f351b18d9d9aff931bee821121be526db
zwl <137636334+Mking47-cpp@users.noreply.github.com>
2024-02-01 14:43:30 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <thread>
+#include <experimental/filesystem>
+//-----------------------------------------------------
+#include "./ThreadPool/Thread.hpp"
+#include "./ThreadPool/ThreadPoolConfig.hpp"
+#include "./ThreadPool/ThreadPool.hpp"
+#include "./CloudBackup/Utility.hpp"
+#include "./CloudBackup/CloudBackupConfig.hpp"
+#include "./CloudBackup/HotManage.hpp"
+#include "./CloudBackup/DataManage.hpp"
+#include "./CloudBackup/Server.hpp"
+#define delayS(s) std::this_thread::sleep_for(std::chrono::seconds(s))
+void run()
+{
+    using namespace zwl;
+    std::thread hotManagerThread([]() -> void
+                   { HotManager::getSingleton()->runHotManage(); });
+    std::thread serverThread([]() -> void
+                                 { Server::getSingleton()->runServer(); });
+    hotManagerThread.join();
+    serverThread.join();
+}
+
+int main()
+{
+    run();
+    
+    return 0;
+}
